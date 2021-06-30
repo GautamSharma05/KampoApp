fd8e0c38d89449a3df1ff64892b31805e8896a83
Gautam Sharma <gautamsharma88540@gmail.com>
2021-06-30 10:55:47 +0530

    Upload Project

---
+++ app/src/main/java/com/example/kampo/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.kampo.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kampo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
