565309423eeb25a1838ea122c2d2f14392ab1e09
Gautam Sharma <gautamsharma88540@gmail.com>
2021-07-08 12:59:55 +0530

    Update Project

--- app/src/main/java/com/example/kampo/SplashScreen/SplashScreen_1.java
+++ app/src/main/java/com/example/kampo/SplashScreen/SplashScreen_1.java
@@ -12,14 +12,22 @@ import android.view.animation.AnimationUtils;
 
 import com.example.kampo.R;
 import com.example.kampo.databinding.FragmentSplashScreen1Binding;
+<<<<<<< Updated upstream
 
 public class SplashScreen_1 extends Fragment {
      FragmentSplashScreen1Binding binding;
      Animation topanim,bottomanim;
+=======
+
+
+public class SplashScreen_1 extends Fragment {
+    FragmentSplashScreen1Binding binding;
+>>>>>>> Stashed changes
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSplashScreen1Binding.inflate(inflater,container,false);
+<<<<<<< Updated upstream
 //        Animation
         topanim= AnimationUtils.loadAnimation(getContext(),R.anim.top_animation);
         bottomanim=AnimationUtils.loadAnimation(getActivity(),R.anim.bottom_animation);
@@ -28,5 +36,8 @@ public class SplashScreen_1 extends Fragment {
         binding.splashScreen1Text.setAnimation(bottomanim);
         return binding.getRoot();
 
+=======
+        return binding.getRoot();
+>>>>>>> Stashed changes
     }
 }
